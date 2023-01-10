39b99537dad8d91c39f1e964affe6ccdf3463c77
mwangiii <mwangi8kevin@gmail.com>
2023-01-10 15:36:31 +0300

    if/else, loops, functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,15 +2,17 @@
 import random
 number = random.randint(-10000, 10000)
 # The string Last digit of, followed by
+string = "Last digit of {} is {} and {}"
 # the number, followed by
 # the string is, followed by the last digit of number, followed by
-# if the last digit is greater than 5: the last digit of the number,is_,and is greater than 5
-last_digit = number % 10
-if last_digit > 5:
-    print('Last digit of {:d} is {:d} is greater than 5'.format(number)(last_digit))
-# if the last digit is 0: the last digit of the number,is_ and is 0
-elif last_digit == 0:
-    print('Last digit of {:d} is {:d} is 0'.format(number)(last_digit))
-# if the last digit is less than 6 and not 0: the last digit of the number,is_,and is less than 6 and not 0
-else:
-    print('Last digit of {:d} is {:d} is less than 6 and not 0'.format(number)(last_digit))
+lastdigit = number % 10
+# if the last digit is greater than 5: the string and is greater than 5
+if lastdigit > 5:
+    print(string.format(number, lastdigit, "is greater than 5"))
+# if the last digit is 0: the string and is 0
+elif lastdigit == 0:
+    print(string.format(number, lastdigit, "is 0"))
+# if the last digit < 6 and not 0: the string and < 6 and not 0
+if lastdigit < 6 and lastdigit != 0:
+    print(string.format(number, lastdigit, "is less than 6 and not 0"))
+# followed by a new line
