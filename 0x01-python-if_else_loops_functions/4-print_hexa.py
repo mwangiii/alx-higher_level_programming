d351118784099e8f452d455e583ca19e31bd514a
mwangiii <mwangi8kevin@gmail.com>
2023-01-11 13:36:32 +0300

    if/else, loops, functions

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,8 +1,3 @@
-# You can only use one print function with string format
-# You can only use one loop in your code
-# You are not allowed to store numbers or strings in a variable
-# You are not allowed to import any module
-
 #!/usr/bin/python3
 for i in range(0, 99):
     print("{:f}={}".format(i, hex(i)))
