66337e68507936655422f262fcf38cb76ef27764
mwangiii <mwangi8kevin@gmail.com>
2023-01-23 22:07:55 +0300

    exception errors

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+        except IndexError:
+            break
+        else:
+            counter += 1
+    print()
+    return counter
