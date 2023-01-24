e44f18250257d6edf87bb7554105f8b64efede5d
mwangiii <mwangi8kevin@gmail.com>
2023-01-24 11:55:55 +0300

    exception errors

--- 3-safe_print_division.py
+++ 3-safe_print_division.py
@@ -2,10 +2,10 @@
 def safe_print_division(a, b):
     try:
         ans = (a/b)
-        print("{} {}".format("Inside result", ans))
 
     except ZeroDivisionError:
-        print("{} {}".format("Inside result:", "None"))
+        ans = None
 
     finally:
+        print("{} {}".format("Inside result", ans))
         return ans
