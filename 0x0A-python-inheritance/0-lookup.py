0f16dba9ee3b272f3800e2328267edbfb2ed909d
mwangiii <mwangi8kevin@gmail.com>
2023-02-08 00:48:44 +0300

    first trial

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Myclass1(object):
+	pass
+
+def lookup(obj):
+        obj = 3
+        return[Myclass1() for i in range(obj)]
+
+class MyClass2(object):
+    my_attr1 = 3
+    def my_meth(self):
+    pass
