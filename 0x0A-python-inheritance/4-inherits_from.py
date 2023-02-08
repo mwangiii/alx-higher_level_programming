c34cf574824b071aec5faf8beae7f9bf4564974a
mwangiii <mwangi8kevin@gmail.com>
2023-02-08 22:17:48 +0300

    python

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""funftion that returns true
+    if obj is instance of a class inherited from
+"""
+
+
+def inherits_from(obj, a_class):
+
+    return isinstance(obj, a_class)
