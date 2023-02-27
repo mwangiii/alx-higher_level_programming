db267752895c321eec7c57021a329864643041b6
mwangiii <mwangi8kevin@gmail.com>
2023-02-28 01:04:57 +0300

    corrections

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,21 +3,17 @@
 
 
 def say_my_name(first_name, last_name=""):
-
     """
-        Print a message with a person's first and last name.
-        Args :
-        first_name: A string representing the person's first name.
-        last_name: A string representing the person's last name (optional).
-        Raises:
-        TypeError: If first_name or last_name is not a string.
+    Print a message with a person's first and last name.
+    Args:
+    first_name: A string representing the person's first name.
+    last_name: A string representing the person's last name (optional).
+    Raises:
+    TypeError: If first_name or last_name is not a string.
     """
-
-    if type(first_name) != str:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-
-    elif type(last_name) != str:
+    elif not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
     else:
-        print("My name is {} {}").format(first_name, last_name)
+        print("My name is {} {}".format(first_name, last_name))
