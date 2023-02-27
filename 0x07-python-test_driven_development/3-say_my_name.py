5929152d1d82cfb44b6ae47062210eaa0f8fb947
mwangiii <mwangi8kevin@gmail.com>
2023-02-28 00:49:12 +0300

    corrections

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,7 +2,8 @@
 def say_my_name(first_name, last_name=""):
     """
         first_name and last_name must be strings otherwise
-        raise TypeError(first_name must be a string or last_name must be a string)
+        raise TypeError(first_name must be a string)
+        or (last_name must be a string)
     """
 
     if first_name != type(str):
