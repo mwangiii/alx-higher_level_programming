2c462397233eab3ce001e3aae4e3ba68079bfebe
mwangiii <mwangi8kevin@gmail.com>
2023-02-27 17:37:35 +0300

    corrections

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,16 +3,16 @@
 This module is composed by a function that divides the numbers of a matrix
 """
 
+
 def matrix_divided(matrix, div):
 
     """ matrix is list of integers or floats
         div is the number in matrix
     """
     if not isinstance(matrix, list):
-    
-        raise TypeError("matrix must be a matrix(list of list) of integers/floats")
+        errmsg = ("matrix must be a matrix(list of list) of integers/floats")
+        raise TypeError(errmsg)
 
-    
     """
     div must be an int or float
     """
@@ -23,15 +23,13 @@ def matrix_divided(matrix, div):
     """
     if div == 0:
         raise ZeroDivisionError("division by zero")
-        
-    """ 
+    """
         each row of a matrix must be the same size
     """
     for element in matrix:
         if element != len(element):
             raise TypeError("Each row of the matrix must have the same size")
-        
     """
-      All elements of the matrix should be divided by div, rounded to 2 decimal places
+      All elements of the matrix should be divided by div,rounded to 2dp
     """
     return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
