b51e6d0db3475f7821e10afa43788f799c47bf21
mwangiii <mwangi8kevin@gmail.com>
2023-04-17 16:04:39 +0300

    js codes

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let [] = process.argv
+
+if (process.argv === 0) {
+    console.log('No argument');
+  } else if (process.argv === 1) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
