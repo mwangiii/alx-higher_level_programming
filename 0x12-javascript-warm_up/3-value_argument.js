6a3742c5f9af80ab69063b0029b12dcf5deda912
mwangiii <mwangi8kevin@gmail.com>
2023-04-18 16:48:36 +0300

    ileiletu

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const [arg = ""] = process.argv.slice(2);
+const [arg = ''] = process.argv.slice(2);
 
 if (arg) {
   console.log(arg);
 } else {
-  console.log("No argument");
+  console.log('No argument');
 }
