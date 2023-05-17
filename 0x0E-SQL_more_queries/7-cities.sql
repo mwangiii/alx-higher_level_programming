0e203b5f724cb5b92d3fb56bd1f92df3b4c2f3e8
mwangiii <mwangi8kevin@gmail.com>
2023-05-17 20:12:19 +0300

    more sql querries

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,11 +6,9 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 USE `hbtn_0d_usa`;
 
 CREATE TABLE IF NOT EXISTS `cities` (
-    `id` INT AUTO_INCREMENT NOT NULL ,
-`state_id` INT NOT NULL REFERENCES `states`,
-`name` VARCHAR(256)NOT NULL,
-PRIMARY KEY (`id`),
-FOREIGN KEY (`state_id`)
-REFERENCES `states`,
-REFERENCES `states`(`state_id`)
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `state_id` INT NOT NULL,
+    `name` VARCHAR(256) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`state_id`) REFERENCES `states` (`id`)
 );
