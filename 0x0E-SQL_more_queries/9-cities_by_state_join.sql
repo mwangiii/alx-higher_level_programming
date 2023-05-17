a2600bf3a047a19de75118264d033103e017ddcd
mwangiii <mwangi8kevin@gmail.com>
2023-05-17 20:25:49 +0300

    more sql queries

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,8 +3,6 @@
 -- Results must be sorted in ascending order by cities.id
 -- You can use only one SELECT statement
 --The database name will be passed as an argument of the mysql command
-
-SELECT cities.id, cities.name, states.name AS state_name
-FROM cities
-JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name
+FROM `cities`
+INNER JOIN `states` ON cities.state_id=states.id;
