906a66fe920bcc9b0a7f19cefd155fa6647c5bbe
mwangiii <mwangi8kevin@gmail.com>
2023-06-12 18:38:57 +0300

    js correction

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+const argscount = process.argv.length - 2;
 
-if (process.argv[0]) {
+if (argscount === 0) {
   console.log('No argument');
-} else if (process.argv[1]) {
+} else if (argscount === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
