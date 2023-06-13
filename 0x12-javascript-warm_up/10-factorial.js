571e53edc3080b87c53cb3d30519c503c598b85a
mwangiii <mwangi8kevin@gmail.com>
2023-06-13 14:42:13 +0300

    js correction

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 
-function factorial(n) {
-    if (isNaN(n)) {
-      return 1;
-    }
-  
-    if (n === 0 || n === 1) {
-      return 1;
-    }
-  
-    return n * factorial(n - 1);
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
   }
-  
-  const input = parseInt(process.argv[2]);
-  const result = factorial(input);
-  console.log(result);
+
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+const input = parseInt(process.argv[2]);
+const result = factorial(input);
+console.log(result);
