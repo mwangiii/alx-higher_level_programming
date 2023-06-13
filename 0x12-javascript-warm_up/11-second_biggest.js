bbdbe9e6729ea121a3f82791d7d0af1655878f80
mwangiii <mwangi8kevin@gmail.com>
2023-06-13 19:35:14 +0300

    js codes

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,23 @@
 #!/usr/bin/node
 
-let numargs = process.argv.length;
+const args = process.argv.slice(2);
 
-if(numargs === 1){
-    console.log(0)
-}else if(numargs === 2){
-    console.log(0)
-}else{
+if (args.length === 0 || args.length === 1) {
+  console.log(0);
+} else {
+  let largest = Number.MIN_SAFE_INTEGER;
+  let secondLargest = Number.MIN_SAFE_INTEGER;
 
+  for (let i = 0; i < args.length; i++) {
+    const num = parseInt(args[i]);
+
+    if (num > largest) {
+      secondLargest = largest;
+      largest = num;
+    } else if (num > secondLargest && num !== largest) {
+      secondLargest = num;
+    }
+  }
+
+  console.log(secondLargest);
 }
