1d008ed98ee8870a6e3360ba7785bf071a224a3f
mwangiii <mwangi8kevin@gmail.com>
2023-06-19 17:20:59 +0300

    js correction

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-exports.nbOccurrences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let count = 0;
 
   for (let i = 0; i < list.length; i++) {
-    if (list[i] === searchElement) {
-      count++;
-    }
+    count = (list[i] === searchElement ? count + 1 : count);
   }
 
   return count;
