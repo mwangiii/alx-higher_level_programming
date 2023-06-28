d98af72e7aa2393a3273ea12b50301ce2cc1e57a
mwangiii <mwangi8kevin@gmail.com>
2023-06-28 20:30:19 +0300

    js advanced debuggign

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 
-// Importing the list array from the 100-data.js file
-const { list } = require('./100-data');
-
-// Computing a new array using the map function
-const newArray = list.map((value, index) => value * index);
-
-// Printing the initial list and the new array
-console.log('Initial List:', list);
-console.log('New Array:', newArray);
+const arr = require('./100-data.js').list;
+console.log(arr);
+const arr2 = arr.map((element, index) => element * index);
+console.log(arr2);
